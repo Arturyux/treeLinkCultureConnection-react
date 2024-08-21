639032cecd4621eced6e13285763e0bcba5a15ac
Arturyux <artur.burlakin@gmail.com>
2024-08-22 01:01:57 +0200

    fix tailwind, set a buttons and containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const links = [
+    {
+      color: "bg-red-300",
+      text: "Bylaws",
+      link: "https://bylaws.cultureconnection.se"
+    },
+    {
+      color: "bg-red-300",
+      text: "Bylaws",
+      link: "https://bylaws.cultureconnection.se"
+    },
+    {
+      color: "bg-red-300",
+      text: "Bylaws",
+      link: "https://bylaws.cultureconnection.se"
+    }
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen bg-yellow-300 flex justify-center items-center'>
+      <div className='w-full max-w-xl h-full bg-black p-6'>
+        {links.map((item, index) => (
+          <p key={index} className={item.color}>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              {item.text}
+            </a>
+          </p>
+        ))}
+        <p className="text-white">asdsa</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
